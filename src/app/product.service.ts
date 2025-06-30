e45648da319f8cc65014d3811f8f81cd19b0ac8c
murtaza <murtaza.almas@ssasoft.com>
2025-06-30 15:52:46 +0500

    add images

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
 export class ProductService {
   public products = [
     {
-      images: ['/images/1.webp', '/images/2.webp', '/images/3.webp'],
-      name: 'HAIDER',
+      images: ['/images/01.webp', '/images/02.webp', '/images/02.webp'],
+      name: 'Kajal Amart Attraction For Men Perfume 100ml',
       price: 'PKR 3,992',
       old: 'PKR 4,990',
       rating: 4.5,
@@ -13,15 +13,34 @@ export class ProductService {
       category: 'New'
     },
     {
-      images: ['/images/4.webp', '/images/5.webp', '/images/6.webp'],
-      name: 'JALAL',
+      images: ['/images/03.webp', '/images/04.webp', '/images/04.webp'],
+      name: 'Aromatic Club Attraction For Men Perfume 100ml',
+      price: 'PKR 3,992',
+      old: 'PKR 4,990',
+      rating: 4.5,
+      notes: 'Top notes: Citrus, Heart: Jasmine, Base: Musk',
+      category: 'New'
+    },
+   
+    {
+      images: ['/images/1.webp', '/images/1.webp', '/images/1.webp'],
+      name: 'Aromatic Club Attraction For Men Perfume 100ml',
+      price: 'PKR 3,992',
+      old: 'PKR 4,990',
+      rating: 4.5,
+      notes: 'Top notes: Citrus, Heart: Jasmine, Base: Musk',
+      category: 'New'
+    },
+    {
+      images: ['/images/2.webp', '/images/2.webp', '/images/2.webp'],
+      name: 'Afnan Supremacy Oud Perfume 100ml',
       price: 'PKR 3,490',
       rating: 4.0,
       notes: 'Top notes: Bergamot, Heart: Rose, Base: Amber',
       category: 'New'
     },
     {
-      images: ['/images/7.webp', '/images/8.webp', '/images/19.webp'],
+      images: ['/images/3.webp', '/images/3.webp', '/images/3.webp'],
       name: 'MIR',
       price: 'PKR 5,121',
       old: 'PKR 5,690',
@@ -30,7 +49,7 @@ export class ProductService {
       category: 'New'
     },
     {
-      images: ['/images/10.webp', '/images/11.webp', '/images/12.webp'],
+      images: ['/images/4.webp', '/images/4.webp', '/images/4.webp'],
       name: 'DURVESH',
       price: 'PKR 6,200',
       rating: 4.2,
@@ -38,7 +57,7 @@ export class ProductService {
       category: 'Men'
     },
     {
-      images: ['/images/13.webp', '/images/14.webp', '/images/15.webp'],
+      images: ['/images/5.webp', '/images/5.webp', '/images/5.webp'],
       name: 'Ishq',
       price: 'PKR 4,490',
       rating: 4.2,
@@ -46,7 +65,7 @@ export class ProductService {
       category: 'Men'
     },
     {
-      images: ['/images/16.webp', '/images/16.webp', '/images/16.webp'],
+      images: ['/images/6.webp', '/images/6.webp', '/images/6.webp'],
       name: 'DURVESH',
       price: 'PKR 6,200',
       rating: 4.2,
@@ -54,7 +73,7 @@ export class ProductService {
       category: 'Men'
     },
     {
-      images: ['/images/17.webp', '/images/17.webp', '/images/17.webp'],
+      images: ['/images/7.webp', '/images/7.webp', '/images/7.webp'],
       name: 'DURVESH',
       price: 'PKR 6,200',
       rating: 4.2,
@@ -62,7 +81,7 @@ export class ProductService {
       category: 'Women'
     },
     {
-      images: ['/images/18.webp', '/images/18.webp', '/images/18.webp'],
+      images: ['/images/8.webp', '/images/8.webp', '/images/8.webp'],
       name: 'DURVESH',
       price: 'PKR 6,200',
       rating: 4.2,
@@ -70,7 +89,7 @@ export class ProductService {
       category: 'Women'
     },
     {
-      images: ['/images/19.webp', '/images/19.webp', '/images/19.webp'],
+      images: ['/images/9.webp', '/images/9.webp', '/images/9.webp'],
       name: 'DURVESH',
       price: 'PKR 6,200',
       rating: 4.2,
@@ -78,7 +97,7 @@ export class ProductService {
       category: 'Kids'
     },
     {
-      images: ['/images/20.webp', '/images/20.webp', '/images/20.webp'],
+      images: ['/images/10.webp', '/images/10.webp', '/images/10.webp'],
       name: 'DURVESH',
       price: 'PKR 6,200',
       rating: 4.2,
@@ -86,7 +105,7 @@ export class ProductService {
       category: 'Kids'
     },
     {
-      images: ['/images/21.webp', '/images/22.webp', '/images/23.webp'],
+      images: ['/images/11.webp', '/images/11.webp', '/images/11.webp'],
       name: 'DURVESH',
       price: 'PKR 6,200',
       rating: 4.2,
@@ -94,7 +113,23 @@ export class ProductService {
       category: 'Sale'
     },
     {
-      images: ['/images/24.webp', '/images/25.webp', '/images/26.webp'],
+      images: ['/images/12.webp', '/images/12.webp', '/images/12.webp'],
+      name: 'DURVESH',
+      price: 'PKR 6,200',
+      rating: 4.2,
+      notes: 'Top notes: Apple, Heart: Peony, Base: Cedarwood',
+      category: 'Sale'
+    },
+    {
+      images: ['/images/13.webp', '/images/13.webp', '/images/13.webp'],
+      name: 'DURVESH',
+      price: 'PKR 6,200',
+      rating: 4.2,
+      notes: 'Top notes: Apple, Heart: Peony, Base: Cedarwood',
+      category: 'Sale'
+    },
+    {
+      images: ['/images/14.webp', '/images/14.webp', '/images/14.webp'],
       name: 'DURVESH',
       price: 'PKR 6,200',
       rating: 4.2,
