fbe23fe58a1059ad57db9bdd3e0fa24435e54173
murtaza <murtaza.almas@ssasoft.com>
2025-09-04 14:03:16 +0500

    add comp

---
+++ src/app/fragrances-collection/fragrances-collection.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fragrances-collection',
+  imports: [],
+  templateUrl: './fragrances-collection.html',
+  styleUrl: './fragrances-collection.css'
+})
+export class FragrancesCollection {
+
+}
