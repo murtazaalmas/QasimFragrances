65ac223519de32881c5507eced748d4fe5c9a84c
murtaza <murtaza.almas@ssasoft.com>
2025-09-04 15:18:54 +0500

    add comp

--- src/app/fragrances-collection/fragrances-collection.ts
+++ src/app/fragrances-collection/fragrances-collection.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fragrances-collection',
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './fragrances-collection.html',
   styleUrl: './fragrances-collection.css'
 })
-export class FragrancesCollection {
+export class FragrancesCollection implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
