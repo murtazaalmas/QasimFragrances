dcc2920c7dba17cde8713c36b610a91dcfc6dfb3
murtaza <murtaza.almas@ssasoft.com>
2025-09-04 15:36:35 +0500

    add comp

---
+++ src/app/loading/loading.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-loading',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './loading.html',
+  styleUrls: ['./loading.css']
+})
+export class LoadingComponent {}
