3b7a13045f2b5494653679eed2ec97f3e979a4ea
murtaza <murtaza.almas@ssasoft.com>
2025-09-04 18:57:58 +0500

    delete reviews file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Home } from './home/home';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'reviews', loadComponent: () => import('./reviews/reviews').then(m => m.Reviews) },
   { path: 'contact', loadComponent: () => import('./contact/contact').then(m => m.Contact) },
   { path: 'about', loadComponent: () => import('./about/about').then(m => m.About) },
   { path: 'collections', loadComponent: () => import('./fragrances-collection/fragrances-collection').then(m => m.FragrancesCollection) },
