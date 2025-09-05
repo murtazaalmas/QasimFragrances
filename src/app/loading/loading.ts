ae83d9db8b6f9bf3ecc6537d7d2d1e9a3621b2e9
murtaza <murtaza.almas@ssasoft.com>
2025-09-05 15:07:44 +0500

    update ui

--- src/app/loading/loading.ts
+++ src/app/loading/loading.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,4 +8,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './loading.html',
   styleUrls: ['./loading.css']
 })
-export class LoadingComponent {}
+export class LoadingComponent {
+  @Input() text: string = 'LOADING';
+}
